d3a334e7d6b79e81388bd5b5bf137401b251f08a
Matthew <105758744+matthew-james-brewer@users.noreply.github.com>
2025-08-09 07:40:29 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# libRattleSnakes
+
+libRattleSnakes is a Objective C Cocoa library for creating textually serializable objects. For more information, see the inline docs, Documentation.md, or compile them with docc.
+
+Requirements:
+ - Only on Cocoa right now.
+
+Make sure to **READ THE LICENSE** before copying or using in your own project!
+
+For an example use of libRattleSnakes, see main.m.
---
+++ main.m
@@ -0,0 +1,37 @@
+#import <Foundation/Foundation.h>
+#import "SNKRattleSnakes.h"
+
+#define chkErr(err) if(!err) { NSLog(@"%@",error); exit(-1); }
+
+int main(int argc, char * argv[]) {
+ @autoreleasepool {
+
+	 NSError * error = nil;
+
+	 SNKColorSnake * cs = [[SNKColorSnake alloc] initWithR:255 G:0 B:128];
+	 
+	 NSLog(@"cs = %@", cs);
+	 
+	 SNKSnakeNSString * ss = [[SNKSnakeNSString alloc] initWithValue:@"Hello, world!" error:&error];
+	 chkErr(ss);
+	 
+	 NSLog(@"ss = %@", ss);
+	 
+	 SNKSnakeNSArray * sa = [[SNKSnakeNSArray alloc] initWithValue:@[cs, ss] error:&error];
+	 chkErr(sa);
+	 
+	 NSLog(@"sa = %@", sa);
+	 
+	 // decoding example
+	 
+	 SNKColorSnake * decode = [[SNKColorSnake alloc] initWithString:@"Color[R=080 G=100 B=250]" error:&error];
+	 chkErr(decode);
+	 
+	 NSLog(@"decode.r = %d, decode.g = %d, decode.b = %d",
+		   [decode getR],
+		   [decode getG],
+		   [decode getB]);
+	 
+	 return 0;
+ }
+}
