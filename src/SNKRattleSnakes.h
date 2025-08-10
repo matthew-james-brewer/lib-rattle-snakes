e425d3eb267a65175f8d0ab8aa37a5da640fedaf
Matthew <105758744+matthew-james-brewer@users.noreply.github.com>
2025-08-09 22:54:33 -0500

    Add files via upload

---
+++ src/SNKRattleSnakes.h
@@ -0,0 +1,20 @@
+//
+//  SNKRattleSnakes.h
+//  RattleSnakes
+//
+//  © 2025 Matthew Brewer
+//
+// This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+
+#ifndef SNKRattleSnakes_h
+#define SNKRattleSnakes_h
+
+#import "SNKSnake.h"
+#import "SNKWrapperSnake.h"
+
+#import "SNKColorSnake.h"
+#import "SNKSnakeNSString.h"
+#import "SNKSnakeNSArray.h"
+
+#endif /* SNKRattleSnakes_h */
